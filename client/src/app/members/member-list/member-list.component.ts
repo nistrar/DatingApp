45298fa9d6a35d8535d9cf59a8165ec60b3fc812
Dan Costea <costea.dan.cristian@gmail.com>
2021-10-15 19:02:33 +0200

    Persistance, Spinner and Caching

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,3 +1,4 @@
+import { Observable } from 'rxjs';
 import { MembersService } from './../../_services/members.service';
 import { Component, OnInit } from '@angular/core';
 import { Member } from 'src/app/_modules/member';
@@ -8,18 +9,10 @@ import { Member } from 'src/app/_modules/member';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
-  members: Member[];
+  members$: Observable<Member[]>;
   constructor(private memberService: MembersService) { }
 
   ngOnInit(): void {
-    this.loadMembers();
+    this.members$ = this.memberService.getMembers();
   }
-
-  loadMembers() {
-    this.memberService.getMembers().subscribe(members => {
-      this.members = members;
-    }
-    )
-  }
-
 }
